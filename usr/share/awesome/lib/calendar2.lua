3745b056613306f371e595de5580a385324eef03
Aphelion <ddarksmith@hotmail.com>
2012-11-08 12:47:06 +0000

    Update usr/share/awesome/lib/calendar2.lua

--- usr/share/awesome/lib/calendar2.lua
+++ usr/share/awesome/lib/calendar2.lua
@@ -64,7 +64,7 @@ function switchNaughtyMonth(switchMonths)
         if (#calendar < 3) then return end
         local swMonths = switchMonths or 1
         calendar[1] = calendar[1] + swMonths
-        calendar[3].box.widgets[2].text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(calendar[1], calendar[2], 2))
+        calendar[3].box.widgets[2].text = string.format('<span font_desc="%s">%s</span>', "DroidSansMono 7", displayMonth(calendar[1], calendar[2], 2))
 end
 
 function addCalendarToWidget(mywidget, custom_current_day_format)
@@ -74,7 +74,7 @@ function addCalendarToWidget(mywidget, custom_current_day_format)
         local month, year = os.date('%m'), os.date('%Y')
         calendar = { month, year,
         naughty.notify({
-                text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(month, year, 2)),
+                text = string.format('<span font_desc="%s">%s</span>', "DroidSansMono 7", displayMonth(month, year, 2)),
                 timeout = 0,
 		fg ="#222222",
 		bg ="#76A3B4",
